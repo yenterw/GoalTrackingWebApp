90c809696456dfb899f716d7ce6163aebb84f8b2
yenterw <yenterw@gmail.com>
2021-04-12 12:14:51 -0500

    Completed(DateTime date) in GoalsController.cs

--- Controllers/GoalsController.cs
+++ Controllers/GoalsController.cs
@@ -58,7 +58,7 @@ namespace GoalTrackingWebApp.Controllers
         // GET: Goals/Completed/5
         public async Task<IActionResult> Completed(DateTime date)
         {
-            return View();
+            return View("Completed", await _context.Goal.Where(j => j.GoalDate.Equals(date)).ToListAsync());
         }
 
         // GET: Goals/Create
