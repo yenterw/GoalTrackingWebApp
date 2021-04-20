b200380cd33dc2d7401661aa03d940e7fa2e6b25
yenterw <yenterw@gmail.com>
2021-04-20 15:58:55 -0500

    Completed page now displays all goals

--- Controllers/GoalsController.cs
+++ Controllers/GoalsController.cs
@@ -58,7 +58,7 @@ namespace GoalTrackingWebApp.Controllers
         // GET: Goals/Completed/5
         public async Task<IActionResult> Completed()
         {
-            return View("Completed", await _context.CompletedGoal.ToListAsync());
+            return View("Index", await _context.Goal.ToListAsync());
         }
 
         // GET: Goals/Create
